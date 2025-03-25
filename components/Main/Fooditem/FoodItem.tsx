6d25e94e21586012bdf521965fe0c4b7799e7ad0
ocsoriram <ossoriram@gmail.com>
2025-03-25 13:02:52 +0000

    feat(FoodItem): 基本の表示を実装した
     細かいレイアウトの調整と、レスポンシブ対応が必要。

--- components/Main/Fooditem/FoodItem.tsx
+++ components/Main/Fooditem/FoodItem.tsx
@@ -1,7 +1,62 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 
 export default function FoodItem() {
-    return (
-        <h1>FoodItem</h1>
-    );
+  type MenuData = {
+    id: string;
+    title: string;
+    imageURL: string;
+    explanation: string;
+  };
+
+  const Menus: MenuData[] = [
+    {
+      id: crypto.randomUUID(),
+      title: "Signature Food",
+      imageURL: "/images/food.jpg",
+      explanation:
+        "サーモンは刺身やカルパッチョで長べるのが実味しいですが、リッチな気分になれるサーモンステーキがおすすめです。サーモンの美味しさを引き立てる2種のソースも絶品です！",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Our Coffee",
+      imageURL: "/images/barista.jpg",
+      explanation:
+        "当店のコーヒー豆が厳選して世界中から買い付けています。そして、そのコーヒー豆のおいしさを最大限に引き出すために自社のロータリーで焙煎せいています。",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Our Food",
+      imageURL: "/images/food3.jpg",
+      explanation:
+        "当店は何よりまずは衛生管理に気をつけています。「安全・安心」をお届けするために日々継続的な努力を惜しみません",
+    },
+  ];
+
+  return (
+    <>
+      {Menus.map((menu) => (
+        <div key={menu.id} className="container">
+          <h3>{menu.title}</h3>
+          <div className="row">
+            <div className="col-6">
+              <div className="ratio ratio-4x3">
+                <Image
+                  src={menu.imageURL}
+                  alt=""
+                  width={200}
+                  height={200}
+                  className="col-md-6"
+                />
+              </div>
+            </div>
+            <div className="col-6">
+              <p>{menu.explanation}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
