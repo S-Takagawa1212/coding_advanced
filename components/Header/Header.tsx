91d18db7e7d90a4a71153b22bf19f0b6fca1f182
ocsoriram <ossoriram@gmail.com>
2025-03-29 12:23:48 +0000

    feat(header): タイトルのフォント変更、ボタンの微調整を行なった。
    コミットの粒度が大きくなってしまったので以後気をつける。

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,10 +7,10 @@ export default function Header() {
   return (
     <>
       <header className="">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar
+        <nav className="navbar navbar-expand-lg pt-0">
+          <div className="container-fluid p-0 ps-md-2">
+            <a className="navbar-brand fs-1 fw-bold" href="#">
+              Casteria
             </a>
             <button
               className="navbar-toggler"
@@ -27,7 +27,7 @@ export default function Header() {
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
                     Home
@@ -35,52 +35,31 @@ export default function Header() {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">
-                    Link
+                    Food
                   </a>
                 </li>
                 <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
+                  <a className="nav-link" href="#">
+                    Drink
                   </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link disabled">Disabled</a>
+                  <a className="nav-link" href="#">
+                    お問い合わせ
+                  </a>
                 </li>
               </ul>
               <form className="d-flex" role="search">
                 <input
-                  className="form-control me-2"
+                  className="form-control me-0"
                   type="search"
                   placeholder="Search"
                   aria-label="Search"
                 />
-                <button className="btn btn-outline-success" type="submit">
+                <button
+                  className="btn btn-outline-none text-black bg-light"
+                  type="submit"
+                >
                   Search
                 </button>
               </form>
