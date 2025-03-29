5611d806a8e95ee18441ac3e8f8fb670e2401f2a
ocsoriram <ossoriram@gmail.com>
2025-03-29 10:07:31 +0000

    feat(header): headerの基本構造を作成した

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,37 @@
-"use client"
+"use client";
 
-import Menu from "./Menu"
-import Searchbar from "./SearchBar"
+import Menu from "./Menu";
+import Searchbar from "./SearchBar";
 
 export default function Header() {
-    return (
-        <>
-          <header className="">
+  return (
+    <>
+      <header className="">
+        <div>
+          <p>here is header</p>
+        </div>
+        <div className="container">
+          <div className="d-flex">
             <div>
-              <p>here is header</p>
+              <h1>Casteria</h1>
             </div>
-            <Menu/>
-            <Searchbar/>
-           </header>
-        </>
-
-    )
+            <nav>
+              <ul>
+                <li>Home</li>
+                <li>Food</li>
+                <li>Drink</li>
+                <li>お問い合わせ</li>
+                <li>
+                  <input type="search" className="search-box" />
+                  <button>Search</button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <Menu />
+        <Searchbar />
+      </header>
+    </>
+  );
 }
