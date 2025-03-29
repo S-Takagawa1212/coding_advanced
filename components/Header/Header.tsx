f3ca98de19d0d4ec312afc9c4056f26991aa6d26
ocsoriram <ossoriram@gmail.com>
2025-03-29 15:48:47 +0000

    feat(header): PCデザイン時のメニューを右に移動させた

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
                     Home
