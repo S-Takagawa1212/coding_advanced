94ae3c94c4f89e2debe2960e44cda84cb1d56446
ocsoriram <ossoriram@gmail.com>
2025-03-30 08:48:07 +0000

    style(foodItem): リストとtype定義をコンポーネントの外に出した

--- components/Main/Fooditem/FoodItem.tsx
+++ components/Main/Fooditem/FoodItem.tsx
@@ -2,38 +2,37 @@
 
 import Image from "next/image";
 
+type MenuData = {
+  id: string;
+  title: string;
+  imageURL: string;
+  explanation: string;
+};
+
+const Menus: MenuData[] = [
+  {
+    id: crypto.randomUUID(),
+    title: "Signature Food",
+    imageURL: "/images/food.jpg",
+    explanation:
+      "サーモンは刺身やカルパッチョで食べるのが実味しいですが、リッチな気分になれるサーモンステーキがおすすめです。サーモンの美味しさを引き立てる2種のソースも絶品です！",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Our Coffee",
+    imageURL: "/images/barista.jpg",
+    explanation:
+      "当店のコーヒー豆が厳選して世界中から買い付けています。そして、そのコーヒー豆のおいしさを最大限に引き出すために自社のロータリーで焙煎せいています。",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Our Food",
+    imageURL: "/images/food3.jpg",
+    explanation:
+      "当店は何よりまずは衛生管理に気をつけています。「安全・安心」をお届けするために日々継続的な努力を惜しみません",
+  },
+];
 export default function FoodItem() {
-  type MenuData = {
-    id: string;
-    title: string;
-    imageURL: string;
-    explanation: string;
-  };
-
-  const Menus: MenuData[] = [
-    {
-      id: crypto.randomUUID(),
-      title: "Signature Food",
-      imageURL: "/images/food.jpg",
-      explanation:
-        "サーモンは刺身やカルパッチョで食べるのが実味しいですが、リッチな気分になれるサーモンステーキがおすすめです。サーモンの美味しさを引き立てる2種のソースも絶品です！",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Our Coffee",
-      imageURL: "/images/barista.jpg",
-      explanation:
-        "当店のコーヒー豆が厳選して世界中から買い付けています。そして、そのコーヒー豆のおいしさを最大限に引き出すために自社のロータリーで焙煎せいています。",
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Our Food",
-      imageURL: "/images/food3.jpg",
-      explanation:
-        "当店は何よりまずは衛生管理に気をつけています。「安全・安心」をお届けするために日々継続的な努力を惜しみません",
-    },
-  ];
-
   return (
     <>
       {Menus.map((menu) => (
