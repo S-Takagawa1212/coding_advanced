d7a91d684ee0fba92dff165d6b5af0e8da421c66
ocsoriram <ossoriram@gmail.com>
2025-03-30 02:29:35 +0000

    refactor(coviddetail): マークアップをh2からh3に変更した

--- components/Main/Covid/CovidDetail.tsx
+++ components/Main/Covid/CovidDetail.tsx
@@ -9,7 +9,8 @@ type props = {
 export default function CovidDetail({ id, title, covidMeasuresList }: props) {
   return (
     <>
-      <h2 className={styles.h2}>◾️{title}</h2>
+      <h3 className={styles.h3}>◾️{title}</h3>
+      {/* 受け取った各項目の説明文をliとして展開する */}
       {covidMeasuresList.map((covidMeasure, index) => {
         return (
           <li key={`${id}` + `${index}`} className="mb-2">
