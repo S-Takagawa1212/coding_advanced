007d9986cc526821e4d51094552709947edae75b
ocsoriram <ossoriram@gmail.com>
2025-03-30 03:57:47 +0000

    feat(coviddetail): クリックでcovidDetailが開閉するよう実装した

--- components/Main/Covid/CovidDetail.tsx
+++ components/Main/Covid/CovidDetail.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import styles from "./Covid.module.css";
 
 type props = {
@@ -7,17 +9,23 @@ type props = {
 };
 
 export default function CovidDetail({ id, title, covidMeasuresList }: props) {
+  // 親コンポーネントでこのコンポーネントをmapで回しているため、各ループ毎に異なるインスタンスが作成されるため、コンポーネント内のstate管理は一つで良い。
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <h3 className={styles.h3}>◾️{title}</h3>
+      <h3 className={styles.h3} onClick={() => setIsOpen((prev) => !prev)}>
+        ◾️{title}
+      </h3>
       {/* 受け取った各項目の説明文をliとして展開する */}
-      {covidMeasuresList.map((covidMeasure, index) => {
-        return (
-          <li key={`${id}` + `${index}`} className="mb-2">
-            ・{covidMeasure}
-          </li>
-        );
-      })}
+      {isOpen &&
+        covidMeasuresList.map((covidMeasure, index) => {
+          return (
+            <li key={`${id}` + `${index}`} className="mb-2">
+              ・{covidMeasure}
+            </li>
+          );
+        })}
     </>
   );
 }
