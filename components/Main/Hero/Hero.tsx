bc4d1f291fa0df69c70514897172110aa4572c71
ocsoriram <ossoriram@gmail.com>
2025-03-31 04:26:42 +0000

    refactore(Hero): コード改修の意図をコメントにて追記

--- components/Main/Hero/Hero.tsx
+++ components/Main/Hero/Hero.tsx
@@ -137,7 +137,11 @@ export default function Hero() {
           ></span>
           <span className="visually-hidden">Next</span>
         </button>
-        <style jsx>{`
+        <style jsx>
+          {/* bootstrapのcarouselによる動的なactive要素の追加は、jsxのclassName={``}記法の中には書けない。自前でisStateによる状態管理を実装し、三項演算子で分岐判断する必要がある。それではbootstrapを使う意味が薄いため、active
+          に関する処理のみ<styles>でスタイルをあてる。 */}
+          {/* TODO　スタイルをmodule.cssに切り出す*/}
+          {`
           .carousel-indicators .active {
             background-color: #007bff !important;
           }
@@ -174,7 +178,7 @@ export default function Hero() {
               padding: 5% 10%;
             }
           }
-        `}</style>
+        `}</styles>
         ;
       </div>
     </>
